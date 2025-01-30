468df8a8a8c9d752671659f0e58eb08b2b7e874b
kim1387 <kevink0@naver.com>
2025-01-30 17:50:02 +0900

    chore: init

---
+++ src/main/java/com/techeer/rocketstarter/RocketStarterApplication.java
@@ -0,0 +1,13 @@
+package com.techeer.rocketstarter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RocketStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RocketStarterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/techeer/rocketstarter/RocketStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.techeer.rocketstarter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RocketStarterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
